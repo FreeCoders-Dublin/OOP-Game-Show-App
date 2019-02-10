84690b3185d3ff779e7d2673f2af0851141a4c72
Luca Tardito <lucatardi@icloud.com>
2019-02-10 15:05:07 +0000

    project works

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,85 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor() {
+         this.missed = 0;
+         this.phrases = this.createPhrases();
+         this.activePhrase = null;
+     }
+
+     createPhrases() {
+         return [
+             new Phrase('take down'),
+             new Phrase('get over'),
+             new Phrase('hang up'),
+             new Phrase('make up'),
+             new Phrase('point out')
+         ];
+     }
+
+     getRandomPhrase() {
+        const phrases = this.phrases;
+        return phrases[Math.floor(Math.random()*phrases.length)];
+     }
+
+     startGame() {
+         document.querySelector("#overlay").style.visibility = "hidden";
+         this.activePhrase = this.getRandomPhrase();
+         this.activePhrase.addPhraseToDisplay();
+     }
+
+    /**
+    * Checks for winning move
+    * @return {boolean} True if game has been won, false if game wasn't
+    won
+    */  
+    checkForWin() {
+        let checker = true;
+        document.querySelectorAll(".letter").forEach(element => {
+            if(element.classList.contains('hide')) {
+                checker = false;
+            }
+        });
+        return checker;
+    }
+
+    removeLife() {
+        document.querySelector('.tries > img[src="images/liveHeart.png"]')
+        .src = "images/lostHeart.png";
+        this.missed ++;
+        if(this.missed > 4) {
+            this.gameOver();
+        }
+    }
+
+    gameOver(gameWon) {
+        const screen = document.querySelector("#overlay");
+        const message = document.querySelector('#game-over-message');
+        if(gameWon) {
+            screen.className = "win";
+            message.innerText = "you Won";
+        } else {
+            screen.className = "lose";
+            message.innerText = "you Lost";
+        }
+        screen.style.visibility = "visible";
+    }
+
+    handleInteraction(button) {
+        button.disabled = true;
+        let buttonText = button.innerText;
+        if(this.activePhrase.checkLetter(buttonText)) {
+            button.classList.add("chosen");
+            this.activePhrase.showMatchedLetter(buttonText);
+            let youWon = this.checkForWin();
+            if(youWon) {
+                this.gameOver(youWon);
+            }
+        } else {
+            button.classList.add("wrong");
+            this.removeLife();
+        }
+    }
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        let phraseArray = this.phrase.split('');
+        let innerHTML = phraseArray.reduce((acc, letter) => letter === ' ' ? 
+            acc + `<li class="space"> </li> \n` : 
+            acc + `<li class="hide letter ${letter}">${letter}</li> \n`
+        , "");
+        document.querySelector("#phrase > ul").innerHTML = innerHTML;
+    }
+    /**
+    * Checks if passed letter is in phrase
+    * @param (string) letter - Letter to check
+    */
+    checkLetter(letter) {
+        return this.phrase.includes(letter);
+    }
+    /**
+    * Displays passed letter on screen after a match is found
+    * @param (string) letter - Letter to display
+    */
+    showMatchedLetter(letter) {
+        document.querySelectorAll('.letter').forEach(element => {
+            if(element.classList.contains(letter)) {
+                element.classList.remove('hide');
+                element.classList.add('show');
+            }
+        });
+    }
+ }
